58704b6d502ba0f752ea9b20fe9afcb2a8722701
jamaloflagos <toyinjamal@gmail.com>
2024-10-28 16:53:20 +0100

    Deployment

--- django_api/wsgi.py
+++ django_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_api project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 app = get_wsgi_application()
 
