807da2402fa4c84a747f2da98daf75aed7b2989f
jamaloflagos <toyinjamal@gmail.com>
2025-01-23 10:20:32 +0100

    Fixing CORS issues

--- api/settings.py
+++ api/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "lms-six-chi.vercel.app"]
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://lms-six-chi.vercel.app/"
+    # "http://localhost:3000",
+    # "https://lms-six-chi.vercel.app/"
+    "*"
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # Allow credentials (cookies, authentication headers)
